76073b7c452810eb06b782b6bd1572354efb9a11
anastasiail <anastasiailasova0@gmail.com>
2025-11-13 16:00:01 +0300

    Update test_graph.c

--- test/test_graph.c
+++ test/test_graph.c
@@ -3,7 +3,7 @@
 #include "../include/graph.h"
 
 void run_tests() {
-    printf("=== Тест 1: Простой путь 0-1-2-3 ===\n");
+    printf("Тест 1: Простой путь 0-1-2-3\n");
     G* g1 = createGraph(4);
     addEdge(g1, 0, 1);
     addEdge(g1, 1, 2);
@@ -19,7 +19,7 @@ void run_tests() {
     
     freeGraph(g1);
 
-    printf("\n=== Тест 2: Граф К3 (треугольник) ===\n");
+    printf("\nТест 2: Граф К3\n");
     G* g2 = createGraph(3);
     addEdge(g2, 0, 1);
     addEdge(g2, 1, 2);
@@ -34,7 +34,7 @@ void run_tests() {
     
     freeGraph(g2);
 
-    printf("\n=== Тест 3: Граф-гантелька ===\n");
+    printf("\nТест 3:\n");
     G* g3 = createGraph(7);
     addEdge(g3, 0, 1);
     addEdge(g3, 1, 2);
@@ -53,7 +53,7 @@ void run_tests() {
     
     freeGraph(g3);
 
-    printf("\n=== Тест 4: Пустой граф ===\n");
+    printf("\nТест 4: Пустой граф\n");
     G* g4 = createGraph(0);
     printf("До:\n");
     printGraph(g4);
@@ -62,7 +62,7 @@ void run_tests() {
     printGraph(g4);
     freeGraph(g4);
 
-    printf("\n=== Тест 5: Граф с одной вершиной ===\n");
+    printf("\nТест 5: Граф с одной вершиной\n");
     G* g5 = createGraph(1);
     printf("До:\n");
     printGraph(g5);
@@ -71,7 +71,7 @@ void run_tests() {
     printGraph(g5);
     freeGraph(g5);
 
-    printf("\n=== Тест 6: Граф с двумя вершинами и ребром ===\n");
+    printf("\nТест 6: Граф с двумя вершинами и ребром\n");
     G* g6 = createGraph(2);
     addEdge(g6, 0, 1);
     printf("До:\n");
@@ -81,7 +81,7 @@ void run_tests() {
     printGraph(g6);
     freeGraph(g6);
 
-    printf("\n=== Тест 7: Звезда ===\n");
+    printf("\nТест 7: Звезда\n");
     G* g7 = createGraph(5);
     addEdge(g7, 0, 1);
     addEdge(g7, 0, 2);
@@ -94,7 +94,7 @@ void run_tests() {
     printGraph(g7);
     freeGraph(g7);
 
-    printf("\n=== Тест 8: Граф K4 ===\n");
+    printf("\nТест 8: Граф K4\n");
     G* g8 = createGraph(4);
     addEdge(g8, 0, 1);
     addEdge(g8, 0, 2);
@@ -109,7 +109,7 @@ void run_tests() {
     printGraph(g8);
     freeGraph(g8);
 
-    printf("\n=== Тест 9: Длинная цепочка ===\n");
+    printf("\nТест 9: Длинная цепочка\n");
     G* g9 = createGraph(10);
     for (int i = 0; i < 9; i++) {
         addEdge(g9, i, i + 1);
@@ -121,7 +121,7 @@ void run_tests() {
     printGraph(g9);
     freeGraph(g9);
 
-    printf("\n=== Тест 10: Граф-цикл ===\n");
+    printf("\nТест 10: Граф-цикл\n");
     G* g10 = createGraph(5);
     for (int i = 0; i < 5; i++) {
         addEdge(g10, i, (i + 1) % 5);
